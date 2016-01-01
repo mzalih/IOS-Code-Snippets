a365fc9ca210754949e397252433c480c45cf5e3
mzalih <salih@toobler.com>
2016-01-01 15:46:35 +0530

    FBLogin Mu Class

--- Fblogin/SocialLgin/SocialLgin/FBLogin/FbLogin.swift
+++ Fblogin/SocialLgin/SocialLgin/FBLogin/FbLogin.swift
@@ -5,8 +5,58 @@
 //  Created by Muhammed Salih on 01/01/16.
 //  Copyright © 2016 Muhammed Salih T A. All rights reserved.
 //
-class FbLogin: NSObject {
+protocol FBLoginDelegate{
+    func onFbLoggedIn(status:Bool)
+}
+class FbLogin:NSObject,FBSDKLoginButtonDelegate,FBLoginDelegate {
     
+    static var instance:FbLogin!
+    var delegate:FBLoginDelegate!
+    var  loginView  = FBSDKLoginButton()
+    static func getInstance()->FbLogin{
+        if(instance == nil){
+            instance = FbLogin()
+            
+            instance.initButton()
+        }
+        return instance
+    }
+    func updateStatus(status:Bool){
+        if(delegate != nil){
+           delegate.onFbLoggedIn(status)
+        }
+    }
+    func initButton(){
+        delegate = self
+        loginView.readPermissions = ["public_profile", "email", "user_friends"]
+        loginView.delegate = self
+    }
+    func loginButtonWillLogin(loginButton: FBSDKLoginButton!) -> Bool {
+        return true
+    }
+    func logIn(withDelegate:FBLoginDelegate){
+        self.delegate = withDelegate
+         loginView.sendActionsForControlEvents(UIControlEvents.TouchUpInside)
+    }
+    static func logIn(withDelegate:FBLoginDelegate){
+         getInstance().logIn(withDelegate);
+           }
+    static func logIn(){
+    getInstance().loginView.sendActionsForControlEvents(UIControlEvents.TouchUpInside)
+    }
+    static func getAssessTocken()->FBSDKAccessToken{
+         return FBSDKAccessToken.currentAccessToken()
+    }
+    static func isLoggedIn()->Bool{
+        if (FBSDKAccessToken.currentAccessToken() != nil)
+        {
+            return true
+        }
+        else
+        {
+            return false
+        }
+    }
     static func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool{
         return FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
     }
@@ -17,6 +67,63 @@ class FbLogin: NSObject {
             openURL: url,
             sourceApplication: sourceApplication,
             annotation: annotation)
+    }
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+        print("User Logged In")
+        
+        if ((error) != nil)
+        {
+            // Process error
+            updateStatus(false)
+
+        }
+        else if result.isCancelled {
+            // Handle cancellations
+            updateStatus(false)
+
+        }
+        else {
+            // If you ask for multiple permissions at once, you
+            // should check if specific permissions missing
+            if result.grantedPermissions.contains("email")
+            {
+                // Do work
+                updateStatus(true)
+
+              //  FbLogin.returnUserData();
+            }
+        }
+    }
+    
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
+        print("User Logged Out")
+        updateStatus(false)
+    }
+    
+    static func returnUserData()
+    {
+        if(FbLogin.isLoggedIn()){
+        let graphRequest : FBSDKGraphRequest = FBSDKGraphRequest(graphPath: "me?fields=id,name,email", parameters: nil)
+        graphRequest.startWithCompletionHandler({ (connection, result, error) -> Void in
+            
+            if ((error) != nil)
+            {
+                // Process error
+                print("Error: \(error)")
+            }
+            else
+            {
+                print("fetched user: \(result)")
+                let userName : NSString = result.valueForKey("name") as! NSString
+                print("User Name is: \(userName)")
+                let userEmail : NSString = result.valueForKey("email") as! NSString
+                print("User Email is: \(userEmail)")
+            }
+        })
+        }
+    }
+    func onFbLoggedIn(status: Bool) {
+        // NO delegate Set
     }
 
 }
--- Fblogin/SocialLgin/SocialLgin/ViewController.swift
+++ Fblogin/SocialLgin/SocialLgin/ViewController.swift
@@ -8,53 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController,FBSDKLoginButtonDelegate {
-
+class ViewController: UIViewController,FBLoginDelegate {
+    //var loginView : FBSDKLoginButton?
+    @IBAction func button(sender: AnyObject) {
+        FbLogin.logIn(self)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        if (FBSDKAccessToken.currentAccessToken() != nil)
-        {
-            // User is already logged in, do work such as go to next view controller.
-        }
-        else
-        {
-            let loginView : FBSDKLoginButton = FBSDKLoginButton()
-            self.view.addSubview(loginView)
-            loginView.center = self.view.center
-            loginView.readPermissions = ["public_profile", "email", "user_friends"]
-            loginView.delegate = self
-        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
-        print("User Logged In")
-        
-        if ((error) != nil)
-        {
-            // Process error
-        }
-        else if result.isCancelled {
-            // Handle cancellations
-        }
-        else {
-            // If you ask for multiple permissions at once, you
-            // should check if specific permissions missing
-            if result.grantedPermissions.contains("email")
-            {
-                // Do work
-            }
-        }
+    //IS THE DELEGATE
+    func onFbLoggedIn(status: Bool) {
+        FbLogin.returnUserData()
     }
-    
-    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
-        print("User Logged Out")
-    }
-
 }
 
