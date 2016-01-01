26c52a4a4d61a8ff937e59574fb659208215f5ba
mzalih <salih@toobler.com>
2016-01-01 11:09:41 +0530

    Changes

---
+++ Fblogin/SocialLgin/SocialLgin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SocialLgin
+//
+//  Created by Muhammed Salih on 01/01/16.
+//  Copyright © 2016 Muhammed Salih T A. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
